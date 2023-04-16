418ecc82c23566c630f5106968119028110a218d
MarianaKely <marianakely.r@gmail.com>
2023-04-15 23:02:39 -0300

    code update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 
 
+import { MongoClient } from "mongodb";
 import express, { json } from 'express';
 import cors from "cors";
+import dayjs from "dayjs";
 import dotenv from "dotenv";
-import * as db from "./configurations.js";
-
+import { userSchema } from "./configurations.js";
 
 dotenv.config();
 
@@ -13,24 +14,79 @@ app.use(cors());
 app.use(json());
 
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const db = mongoClient.db();
 
-app.post("/participants", async (req, res) => {
 
-  const participant = req.body;
-  const chatStatus = await db.chatEntry(participant);
-  return res.status(chatStatus.code).send(chatStatus.message);
+
+async function uolAPi() {
+
+  try {
+
+    await mongoClient.connect();
+    
+  } catch (error) {
+    
+  }
+
+
+  app.post("/participants", async (req, res) => {
+
+    const { name } = req.body;
+    const chatEntry = userSchema.validate({ name });
+    const lastStatus = Date.now();
+
+    if (chatEntry.error) {
+      return res.status(422).send("ERROR");
+
+    }
+
+    try {
+
+      const changeUser = await db.collection("participants").findOne({ name: name });
+
+      if (changeUser) {
+
+        return res.status(409).send("ERROR");
+
+      }
+
+      await db.collection("participants").insertOne({ name, lastStatus });
+      await db.collection("messages").insertOne({
+
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("hh:mm:ss"),
+
+      });
+
+      res.sendStatus(201);
+      
+    } catch (error) {
+      
+      res.status(500).send("ERROR");
+
+    }
+  });
+
+
+  app.get("/participants", async (_, res) => {
+
+    const participant = await db.collection("participants").find().toArray();
+    res.send(participant);
+
+  });
+
+
+
+  const PORT = 5000;
+
+  app.listen(PORT, () => { console.log(`HI, ITS ME!!!`);
 
 });
 
-app.get("/participants", async (req, res) => {
+}
 
-  const theParticipants = await db.chatParticipants();
-  res.status(theParticipants.code).send(theParticipants.data);
-
-});
-
-
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`HI, ITS ME!!!`));
+uolAPi();
